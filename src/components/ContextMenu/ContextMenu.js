90c17d2a364b506bd6ee0fd563fa9cb76681c868
Nofar Baranes <nofar@reallygood.co.il>
2020-03-15 17:26:31 +0200

    Removed unnecessary code

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -35,6 +35,56 @@ export default function ContextMenu(props) {
   );
 }
 
+const getAdditionalStyles = props => {
+  const { disabled, saperator, subMenu } = props;
+
+  const subMenuStyles = subMenu
+    ? ` &:before {
+              content: '';
+              display: block;
+              height: 0;
+              width: 0;
+              border-top: 5px solid transparent;
+              border-bottom: 5px solid transparent;
+              border-left: 5px solid ${disabled ? '#ddd' : '#000'};
+              position: absolute;
+              right: 3px;
+              top: 50%;
+              transform: translateY(-50%);
+        }`
+    : '';
+
+  const disabledStyles = disabled
+    ? `
+      color: ${disabled ? '#ddd' : '#000'};
+      pointer-events: none;
+    `
+    : '';
+
+  const saperatorStyles = saperator
+    ? ` &:after {
+              content: '';
+              display: block;
+              height: 1px;
+              width: 100%;
+              background: #ddd;
+              position: absolute;
+              right: 0;
+              left: 0;
+              bottom: -5px;
+            }
+    
+            margin-bottom: 10px;
+        `
+    : '';
+
+  return `
+      ${subMenuStyles}
+      ${disabledStyles}
+      ${saperatorStyles}
+    `;
+};
+
 const StyledContextMenu = styled('ul')`
   z-index: 50;
   background-color: #fff;
@@ -45,102 +95,6 @@ const StyledContextMenu = styled('ul')`
   border: 1px solid #a7a394;
   box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
-
-const getSapetatorStyles = saperator =>
-  saperator
-    ? `
-          &:after {
-            content: '';
-            display: block;
-            height: 1px;
-            width: 100%;
-            background: #000;
-            position: absolute;
-            right: 0;
-            left: 0;
-            bottom: -5px;
-          }
-  
-          margin-bottom: 10px;
-        `
-    : '';
-
-const getSubMenuArrowStyles = subMenu =>
-  subMenu
-    ? `
-      &:before {
-        content: '';
-        display: block;
-        height: 0;
-        width: 0;
-        border-top: 5px solid transparent;
-        border-bottom: 5px solid transparent;
-        border-left: 5px solid #000;
-        position: absolute;
-        right: 3px;
-        top: 50%;
-        transform: translateY(-50%);
-      }
-    `
-    : '';
-
-const getDisabledStyles = disabled =>
-  disabled
-    ? `
-        opacity: 0.4;
-        pointer-events: none;
-    `
-    : '';
-
-const getAdditionalStyles = props => {
-  const { disabled, saperator, subMenu } = props;
-
-  const subMenuStyles = subMenu
-    ? ` &:before {
-            content: '';
-            display: block;
-            height: 0;
-            width: 0;
-            border-top: 5px solid transparent;
-            border-bottom: 5px solid transparent;
-            border-left: 5px solid ${disabled ? '#ddd' : '#000'};
-            position: absolute;
-            right: 3px;
-            top: 50%;
-            transform: translateY(-50%);
-      }`
-    : '';
-
-  const disabledStyles = disabled
-    ? `
-    color: ${disabled ? '#ddd' : '#000'};
-    pointer-events: none;
-  `
-    : '';
-
-  const saperatorStyles = saperator
-    ? ` &:after {
-            content: '';
-            display: block;
-            height: 1px;
-            width: 100%;
-            background: #ddd;
-            position: absolute;
-            right: 0;
-            left: 0;
-            bottom: -5px;
-          }
-  
-          margin-bottom: 10px;
-      `
-    : '';
-
-  return `
-    ${subMenuStyles}
-    ${disabledStyles}
-    ${saperatorStyles}
-  `;
-};
 
 const StyledContextMenuItem = styled('li')`
   position: relative;
@@ -168,6 +122,7 @@ const StyledContextMenuItem = styled('li')`
       color: rgb(255, 255, 255);
     }
 
+    /** before: submenuArrow */
     &&:before {
       border-left-color: #fff;
     }
