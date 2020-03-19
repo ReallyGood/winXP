d003740f921ba46a625ff4dbabf8b8d82d3cfb68
Nofar Baranes <nofar@reallygood.co.il>
2020-03-19 15:25:52 +0200

    Work on displayProperties desktopTab content

--- src/WinXP/apps/DisplayProperties/Tabs/DesktopTab.js
+++ src/WinXP/apps/DisplayProperties/Tabs/DesktopTab.js
@@ -1,24 +1,153 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Button from 'components/Button';
+import display from '../../../../assets/displayProperties/display.png';
+import noneIcon from '../../../../assets/displayProperties/icons/none.png';
+import imageIcon from '../../../../assets/displayProperties/icons/image.png';
 
 export default function DesktopTab(props) {
   const { title, data, dataChanged } = props;
-  const { counter } = data;
-  const [count, setCount] = useState(counter);
-  const handleOnChanged = () => {
-    const newCount = count + 1;
-    const newData = { ...data, ...{ counter: newCount } };
-    dataChanged(newData);
-    setCount(newCount);
-  };
+  const {
+    backgroundSrc,
+    solidColor,
+    backgroundIsImage,
+    backgroundOptions,
+  } = data;
+  // const [count, setCount] = useState(counter);
+  // const handleOnChanged = () => {
+  //   const newCount = count + 1;
+  //   const newData = { ...data, ...{ counter: newCount } };
+  //   dataChanged(newData);
+  //   setCount(newCount);
+  // };
 
   return (
-    <div>
-      <Button
-        action={data}
-        onClick={handleOnChanged}
-      >{`change ${title} data`}</Button>
-      {count}
-    </div>
+    <Container>
+      <DisplayWrapper className="display-warapper">
+        <Display className="display" src={display} alt="" />
+        {backgroundIsImage ? (
+          <Image className="background" src={backgroundSrc} alt="" />
+        ) : (
+          <SolidColor color={solidColor}></SolidColor>
+        )}
+      </DisplayWrapper>
+      <DisplaySettings className="display-settings">
+        <BackgroundSelection>
+          <span className="header">Background:</span>
+          <Select size="5">
+            {backgroundOptions.map(option => {
+              return (
+                <Option
+                  value={option.src}
+                  icon={Boolean(option.src) ? imageIcon : noneIcon}
+                >
+                  {option.name}
+                </Option>
+              );
+            })}
+          </Select>
+        </BackgroundSelection>
+        <BackgroundOptions>options</BackgroundOptions>
+      </DisplaySettings>
+    </Container>
   );
 }
+
+const Container = styled('div')`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 2px;
+`;
+
+const DisplayWrapper = styled('div')`
+  position: relative;
+  width: 195px;
+  height: 170px;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  padding: 16px 16px 35px 13px;
+`;
+
+const Display = styled('img')`
+  width: 100%;
+  height: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Image = styled('img')`
+  width: 100%;
+  height: auto;
+  z-index: 2;
+  object-fit: cover;
+`;
+
+const SolidColor = styled('div')`
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background-color: ${props => props.color || '#000'};
+`;
+
+const DisplaySettings = styled('div')`
+  padding-top: 5px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 4.3fr 1fr;
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  grid-row-gap: 0px;
+`;
+
+const BackgroundSelection = styled('div')`
+  grid-area: 1 / 1 / 6 / 2;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 20px 1fr;
+
+  .header {
+    font-size: 12px;
+  }
+`;
+
+const BackgroundOptions = styled('div')`
+  grid-area: 1 / 2 / 6 / 3;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 20px, 1fr;
+`;
+
+const Select = styled('select')`
+  overflow-y: scroll;
+  height: 118px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Option = styled('option')`
+  position: relative;
+  height: 15px;
+  padding: 2px 0 3px 25px;
+  font-size: 12px;
+
+  &&:before {
+    content: '';
+    width: 20px;
+    height: 20px;
+    background-image: url(${props => props.icon});
+    background-repeat: no-repeat;
+    background-size: 80%;
+    background-position: 50% 50%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 2px;
+    bottom: 0;
+  }
+`;
--- src/WinXP/apps/DisplayProperties/Tabs/index.js
+++ src/WinXP/apps/DisplayProperties/Tabs/index.js
@@ -6,6 +6,8 @@ import ScreenSaverTab from './ScreenSaverTab';
 import AppearanceTab from './AppearanceTab';
 import SettingsTab from './SettingsTab';
 import { Tabs, Tab, TabList, TabPanel } from '../../../../components/Tabs';
+import defaultBackground from '../../../../assets/displayProperties/backgrounds/default.jpg';
+import reallyGoodBackground from '../../../../assets/displayProperties/backgrounds/really-good.png';
 
 export const defaultPropertiesTabs = [
   {
@@ -19,8 +21,59 @@ export const defaultPropertiesTabs = [
     title: 'Desktop',
     component: DesktopTab,
     data: {
-      counter: 0,
       id: 'desktop',
+      backgroundIsImage: true,
+      backgroundSrc: reallyGoodBackground,
+      solidColor: 'red',
+      backgroundOptions: [
+        {
+          name: '(None)',
+          src: null,
+        },
+        {
+          name: 'windows',
+          src: defaultBackground,
+        },
+        {
+          name: 'really-good',
+          src: reallyGoodBackground,
+        },
+        {
+          name: 'bg1',
+          src:
+            'https://www.kellsolutions.com/wp-content/uploads/2019/07/1564233245-678x381.jpg',
+        },
+        {
+          name: 'bg2',
+          src:
+            'https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        },
+        {
+          name: 'bg3',
+          src:
+            'https://cdn.shopify.com/s/files/1/2440/7149/products/WH401_800x.jpg?v=1579885607',
+        },
+        {
+          name: 'bg4',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT1wpFtY1HNEUdmCQj8iN0hFxWVFGTWhs-YftqfpBn-qJHtjC7R',
+        },
+        {
+          name: 'bg5',
+          src:
+            'https://images.befunky.com/wp/wp-2016-03-blur-background-featured-1.jpg?auto=format&fm=jpg&q=75&w=880&ixlib=js-1.4.1',
+        },
+        {
+          name: 'bg6',
+          src:
+            'https://wallup.net/wp-content/uploads/2016/01/153280-Fractalius-leopard-black_background-animals-digital_art-748x421.jpg',
+        },
+        {
+          name: 'bg7',
+          src:
+            'https://img5.goodfon.com/wallpaper/nbig/7/65/koshka-kot-vzgliad-morda-fon-portret-khvost-priroda-razmytie.jpg',
+        },
+      ],
     },
   },
   {
