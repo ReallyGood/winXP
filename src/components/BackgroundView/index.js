1472957ccb27c938df20197f4bd0f0269c840007
Tomer Matmon <tomer.matmon@reallygood.co.il>
2021-04-06 16:13:48 +0300

    Change BackgroundView.

--- src/components/BackgroundView/index.js
+++ src/components/BackgroundView/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 function BackgroundView({ background }) {
-  const { type, size, image, color } = background;
-  return <Background type={type} size={size} image={image} color={color} />;
+  const { position, image, color } = background;
+  return <Background position={position} image={image} color={color} />;
 }
 
 const Background = styled.div`
   width: 100%;
   height: 100%;
   pointer-events: none;
-  ${({ type, image, color, size }) => {
+  ${({ image, color, position }) => {
     return `background-image: url(${image});
       background-color: ${color}
       background-repeat: no-repeat;
       background-position: center;
-      background-size: ${size};`;
+      background-size: 100% 100% ;`;
   }}
 `;
 // repeat? return repeat and position unset/top left : return no-repeat and position center
